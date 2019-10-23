741c33cb0777fac6ad3b957cec076d5d6ea4d615
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-22 17:47:42 -0700

    Almost done with day1 again

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Note(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField(max_length=500, blank=True)
+	sidenote = models.TextField(max_length=50, blank=True)
+	# Adding a key recorder
+	id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+	
